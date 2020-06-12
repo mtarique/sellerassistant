e4e187e615f057daabaf38888e67a27b30b2f7cf
rtgi-developers <58165254+rtgi-developers@users.noreply.github.com>
2020-06-12 15:21:08 +0530

    Improvement: Removed the gap beween topnav and leftnav.

--- application/views/templates/topnav.php
+++ application/views/templates/topnav.php
@@ -1,7 +1,7 @@
-<nav class="navbar fixed-top navbar-dark bgdark" style="background-color: #000000 !important;">
-    <div class="container-fluid p-0">
-        <a href="#" class="navbar-brand p-0">
-            <img src="<?php echo base_url('assets/img/logo/sa_logo_horizontal_blue.jpg'); ?>" class="img-fluid ml-n1" alt="site-logo" width="180">
+<nav class="navbar fixed-top navbar-dark" style="background-color: #000000 !important;">
+    <div class="container-fluid px-0">
+        <a href="#" class="navbar-brand py-0">
+            <img src="<?php echo base_url('assets/img/logo/sa_logo_horizontal_blue.jpg'); ?>" class="img-fluid ml-n1" alt="site-logo" width="190">
         </a>
         <div class="d-flex flex-row ml-auto">
             <!-- <div class="pr-3">
